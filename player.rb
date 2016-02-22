1cbaaeaa5b33d2a49e4844a1d2cb905d79068850
GavMan1995 <gavyncaldwell@gmail.com>
2016-02-22 13:48:30 -0700

    fixed freaking errors in player class

--- player.rb
+++ player.rb
@@ -9,10 +9,6 @@ class Player
 
   def wallet
     	@amount = rand(100..1000)
-<<<<<<< HEAD
       puts "your starting amount is: #{@amount}"
     end
-=======
   end
->>>>>>> 3bba217d473173f6b2eea7a10a2d176ed0c658fb
-end
