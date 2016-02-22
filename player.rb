325d8edb2a49629ada3c095d4ba520038922267a
Parker Smith <linuxlovr1@gmail.com>
2016-02-22 11:24:36 -0700

    third player commit

--- player.rb
+++ player.rb
@@ -1,7 +1,10 @@
 class Player
 	def initialize
+		@player_name = gets.strip
 		puts "Your player name is #{@player_name}"
+		wallet
     end
+    
     def wallet
     	@amount = rand(100..1000)
     end
