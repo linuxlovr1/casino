d5925744288406e153f10a695ac8bd6a84558840
GavMan1995 <gavyncaldwell@gmail.com>
2016-02-22 10:58:06 -0700

    added menu method

--- casino.rb
+++ casino.rb
@@ -1,6 +1,25 @@
 class Casino
+
   def initialize
     puts "---Welcome to the Ruby Casino---\nplease type your name"
     print ">"
+    @player_name = gets.strip
+    menu
   end
+
+  def menu
+    puts "okay #{@player_name} lets start. what would you like to do?"
+    puts "1. See Wallet\n2. Play Hi-Lo\n3. Play Slots\n4. Exit"
+    @where = gets.strip.to_i
+    case @where
+      when 1
+        #Wallet
+      when 2
+        #Hi-Lo
+      when 3
+        #Slots
+      when 4
+        "Thank you come again, your final amount was #{@amount}"
+  end
+
 end
