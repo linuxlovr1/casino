d2d84a6d5ee236ff03bc88ee23b8cfb06f231fb8
ottjw <ottjw@yahoo.com>
2016-02-24 00:28:02 +0000

    blackjack changes deck changes

--- black_jack.rb
+++ black_jack.rb
@@ -4,6 +4,7 @@ class BlackJack
   def initialize(player)
     @player = player
     @deck = Deck.new
+    @hand = []
     puts "Welcom to Ruby BlackJack!"
     start
   end  
@@ -12,18 +13,56 @@ class BlackJack
       puts "How much would you like to bet?"
       puts "You have $#{@player.amount}"
       bet = gets.strip
+      card = @deck.deal
+      play_bj
+      
     end 
 
 
-      def deal
-      @face = "jqk"
-      @ace = "a"
-      rand_card = @deck.ranks.sample
-      if rand_card.include?(@face)
-        @card_value = 10
-        puts @card_value
-      elsif rand_card.include?(@ace)
-        puts @card_value = 1
+      def play_bj
+        puts "Deal Cards? y/n"
+        bj_yn = gets.strip.downcase
+        if bj_yn == "y"
+          yes_deal
+        elsif "n"
+          puts "thanks for playing!"
+        else 
+          puts "what did you say?"
+          play_bj
+        end
       end
-    end
+        
+      def yes_deal
+        @hand.push(@deck.deal.bjv, @deck.deal.bjv)
+        @first_deal = "#{@hand[0]} and #{@hand[1]}"
+        puts @first_deal
+        @hand_total = @hand.reduce(:+)
+        puts "(#{@hand_total})\n\n"
+        hit_or_stay
+        
+      end
+      
+      def hit_or_stay
+        puts "what would you like to do?\n1.Hit\n2.Stay"
+        hitstay = gets.strip.to_i
+         if hitstay == 1
+           hit
+         elsif hitstay == 2 
+           stay
+         else
+           "didn't get that"
+           hit_or_stay
+         end  
+      end  
+        
+      def hit
+        new_card = @deck.deal.bjv
+        puts new_card
+        @hand << new_card
+        puts @first_deal + "#{@hand_total}"
+        hit_or_stay
+      end  
+
+
 end
+
--- deck.rb
+++ deck.rb
@@ -1,11 +1,15 @@
 class Card
  # Getter and Setter methods for rank, suit and color
- attr_accessor :rank, :suit, :color
+ attr_accessor :rank, :suit, :bjv
  # Gets called when you call the new method to create an instance
- def initialize(rank, suit, color)
+ def initialize(rank, bjv, suit, color)
    @rank = rank
+   @bjv = bjv
    @suit = suit
    @color = color
+ end
+ def to_s
+   "#{@rank} of #{@suit}" 
  end
 end
 
@@ -15,20 +19,27 @@ class Deck
  # Gets called when you call the new method to create an instance
  def initialize
    @ranks = %w(A 2 3 4 5 6 7 8 9 10 J Q K)
+   @bjv = [11, 2,3,4,5,6,7,8,9,10,10,10,10]
    @suits = %w(Spades Diamonds Clubs Hearts)
    @cards = []
    generate_deck
+   @cards.shuffle!
  end
 
  def generate_deck
+  @cards = []
    @suits.each do |suit|
      @ranks.size.times do |i|
        # Ternary Operator
        color = (i % 2 == 0) ? 'Black' : 'Red'
-       @cards << Card.new(@ranks[i], suit, color)
+       @cards << Card.new(@ranks[i], @bjv[i], suit, color)
      end
    end
  end
+ 
+ def deal
+   @cards.pop
+ end 
 end
 
-d = Deck.new
+
